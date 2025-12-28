2a77a660e0805ad04b17bc01279df99a30db602d
Francis Kangaty <franciskangaty@gmail.com>
2025-12-28 18:13:21 +0300

    first commit

---
+++ src/main/java/com/ratel/rental_manager/RentalManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ratel.rental_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalManagerApplication.class, args);
+	}
+
+}
